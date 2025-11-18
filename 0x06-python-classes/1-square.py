951dede6fc776af8e111c70698a9f28a97a4c020
mechanicslab <enividibgihsa@gmail.com>
2025-11-18 05:25:05 +0000

    Add 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" 1-square.py """
+
+
+class Square:
+
+    """ A square class with one private instance attribute """
+
+    def __init__(self, size):
+        self._size = size
