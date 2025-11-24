dc9ec80708440d577133b1715cbb9cc97b1d1935
mechanicslab <enividibgihsa@gmail.com>
2025-11-24 22:40:14 +0000

    Amend 2-square.py to make raising TypeError come first and before ValueError

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,7 +10,7 @@ class Square:
         """ the class constructor method """
 
         self.__size = size
-        if size < 0:
-            raise ValueError("size must be >= 0")
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
