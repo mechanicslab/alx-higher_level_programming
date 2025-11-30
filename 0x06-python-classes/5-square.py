9c1b110ef3a638f87d5212c844b63b9d74f8d5a2
Mawunyo on wsl2 <dmashigbi@gmail.com>
2025-11-30 22:30:59 +0000

    Amend 5-square.py to contain actual script

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,1 +1,49 @@
 #!/usr/bin/python3
+""" A module defining a square class """
+
+
+class Square:
+    """ A square class """
+
+    def __init__(self, size=0):
+
+        """ class constructor method """
+
+        self.__size = size
+
+    @property
+    def size(self):
+
+        """ A property method to retrieve size"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        """ A property setter """
+
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+
+        """ Calculate the square's area """
+
+        return self.__size * self.__size
+
+    def my_print(self):
+
+        """ A public method to print the squares """
+
+        dummy_size = self.__size
+        if self.__size == 0:
+            print()
+        while dummy_size > 0:
+            for index in range(self.__size):
+                print("#", end='')
+            print()
+            dummy_size = dummy_size - 1
